5416b4fa25db5bd279701bc0e207f4a62006f634
Omar Mahfooz <omar@Omars-MacBook-Air.local>
2023-09-18 19:55:14 +0100

    added first urls to users app

--- realworld/realworld/urls.py
+++ realworld/realworld/urls.py
@@ -17,6 +17,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from users.views import UserRegistrationAPIView
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("users/", UserRegistrationAPIView.as_view(), name="register"),
 ]
--- realworld/users/views.py
+++ realworld/users/views.py
@@ -1,3 +1,47 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+from rest_framework import viewsets
+from rest_framework.permissions import AllowAny, IsAuthenticated
+from .models import User
+from users.serializers import RegistrationResponseSerializer, UserRegistrationSerializer
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from django.contrib.auth import authenticate
+from rest_framework.authtoken.models import Token
+from rest_framework.decorators import action
+from rest_framework_simplejwt.authentication import JWTAuthentication
+from rest_framework.views import APIView
+from rest_framework import status
+from rest_framework_simplejwt.tokens import RefreshToken
+from rest_framework.viewsets import ViewSet
 
-# Create your views here.
+
+class UserRegistrationAPIView(APIView):
+    permission_classes = [AllowAny]
+
+    def post(self, request):
+        serializer = UserRegistrationSerializer(data=request.data.get('user'))
+
+        if serializer.is_valid():
+            user = serializer.save()
+
+            refresh = RefreshToken.for_user(user)
+            access_token = str(refresh.access_token)
+
+            response_data = {
+                'user': RegistrationResponseSerializer({
+                    'email': user.email,
+                    'username': user.username,
+                    'token': access_token,
+                }).data
+            }
+            return Response(response_data, status=status.HTTP_201_CREATED)
+
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class UserViewSet(ViewSet):
+    @action(detail=False, methods=['POST'])
+    def register(self, request):
+        user_registration_view = UserRegistrationAPIView.as_view()
+        return user_registration_view(request=request)._data
